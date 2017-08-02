dc7c820cc6752e8c8d9bda1b7d3bcaab7e216bc8
Jonathan Lloyd <jonathan@thisisjonathan.com>
2017-08-02 16:25:11 +0100

    CDN upload

--- textsync.js
+++ textsync.js
@@ -13848,7 +13848,7 @@ function createAnonName() {
     return capitalize(adjective + ' ' + noun);
 }
 function randomChoice(list) {
-    var randomIndex = Math.round(Math.random() * list.length);
+    var randomIndex = Math.round(Math.random() * list.length - 1);
     return list[randomIndex];
 }
 function capitalize(text) {
